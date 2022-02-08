4a7572165a504675a7286d040c4907f376aa4275
AyubBR <fabioayubrasil@gmail.com>
2022-02-08 20:45:58 -0300

    Initial commit

---
+++ pooCalculaIdade/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    public class Pessoa
+    {
+
+        private string nome;
+
+        public string Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value.ToUpper(); }
+        }
+
+        private int anoNascimento;
+
+        public int AnoNascimento
+        {
+            get { return this.anoNascimento; }
+            set { this.anoNascimento = value; }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome:" + this.Nome);
+            Console.WriteLine("Ano de Nascimento:" + this.AnoNascimento);
+            Console.WriteLine("Idade:" + CalcularIdade());
+        }
+        private int CalcularIdade()
+        {
+            var data = DateTime.Now;
+            var ano = data.Year;
+            int idade = ano - this.AnoNascimento;
+            return idade;
+        }
+
+    }
+
+
+}
---
+++ pooCalculaIdade/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace pooCalculaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calcular idade");
+            Pessoa person = new Pessoa();
+            Console.WriteLine("Digite o nome da pessoa:");
+            person.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o ano de nascimento da pessoa:");
+            person.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+            person.ExibirDados();
+
+        }
+    }
+}
