e34f1da3c9c864f8384a6151e8d5591c68ef7abb
AyubBR <fabioayubrasil@gmail.com>
2022-02-17 19:59:53 -0300

    + Animais, + Área Retângulo

---
+++ Área de Retângulo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AreaRetangulo
+{
+    public class AreaRetangulo
+    {
+        static void Main (string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Programa Cálculo Área");
+            Console.WriteLine("Infome o valor da base do retângulo");
+            retangulo.Baseretangulo = Convert.T (Console.ReadLine());
+            Console.WriteLine("Infome o valor da altura do retângulo");
+            retangulo.Alturaretangulo = Console.ReadLine();
+        }
+    }
+}
---
+++ Área de Retângulo/Retângulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    public class Retangulo
+    {
+        //Contrutores
+        public Retangulo()
+        {
+            float Baseretangulo = 0;
+            float Alturaretangulo = 0;
+        }
+
+        public Retangulo(float baseretangulo, float alturaretangulo)
+        {
+            float Baseretangulo = baseretangulo;
+            float Alturaretangulo = alturaretangulo;
+        }
+
+        //Propriedades
+        private float baseretangulo;
+
+        public float Baseretangulo
+        {
+            get { return baseretangulo; }
+            set { baseretangulo = value; }
+        }
+
+        private float alturaretangulo;
+
+        public float Alturaretangulo
+        {
+            get { return alturaretangulo; }
+            set { alturaretangulo = value; }
+        }
+
+        private float area;
+
+        public float Area
+        {
+            get { return Baseretangulo*Alturaretangulo; }
+            set { area = value; }
+        }
+
+
+        //Métodos
+        public void ExibirDados ()
+        {
+            Console.WriteLine("Base: " + Baseretangulo);
+            Console.WriteLine("Altura: " + Alturaretangulo);
+            Console.WriteLine("Area: " + Area);
+        }
+
+    }
+}
